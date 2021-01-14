3ce54bf37e9d5062e31dd3414973cb63bf61d72a
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-14 02:04:29 +0000

    Finished implementing function with edge cases

--- fetcher.js
+++ fetcher.js
@@ -4,8 +4,21 @@ const fs = require('fs');
 const url = process.argv.slice(2)[0];
 const filePath = process.argv.slice(2)[1];
 
+
 request(url, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    process.exit();
+  }
+  if (response.statusCode !== 200) {
+    console.log('Error: ', response.statusCode);
+    process.exit();
+  }
   fs.writeFile(filePath, body, (error) => {
+    if (`${filePath[0]}` !== '.') {
+      console.log('Invalid file path')
+      process.exit();
+    }
     const stats = fs.statSync(`${filePath}`);
     const fileSize = stats.size;
     if (error) {
@@ -14,6 +27,4 @@ request(url, (error, response, body) => {
     console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
   })
 })
-
-
 
